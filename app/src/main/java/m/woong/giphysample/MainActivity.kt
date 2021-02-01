4af0b740ed5203c2e5a5d89c2d1ae27e57deb4e4
dlqkfzlr <hisunwoong@gmail.com>
2021-02-01 16:19:33 +0900

    Initial commit

---
+++ app/src/main/java/m/woong/giphysample/MainActivity.kt
@@ -0,0 +1,18 @@
+package m.woong.giphysample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import m.woong.giphysample.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/m/woong/giphysample/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package m.woong.giphysample.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
