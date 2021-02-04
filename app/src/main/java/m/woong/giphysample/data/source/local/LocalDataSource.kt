b2b511da2bbb1dcb3336ec22b0aa7edf09a5853f
dlqkfzlr <hisunwoong@gmail.com>
2021-02-05 03:00:59 +0900

    Modify LocalDataSource.kt & LocalDataSourceImpl.kt

--- app/src/main/java/m/woong/giphysample/data/source/local/LocalDataSource.kt
+++ app/src/main/java/m/woong/giphysample/data/source/local/LocalDataSource.kt
@@ -1,4 +1,23 @@
 package m.woong.giphysample.data.source.local
 
+import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
+import androidx.paging.PagingSource
+import kotlinx.coroutines.flow.Flow
+import m.woong.giphysample.data.source.local.entity.Gif
+import m.woong.giphysample.data.source.local.entity.RemoteKeys
+
 interface LocalDataSource {
+
+    /* GifDao */
+    suspend fun saveGifs(gifs: List<Gif>)
+    fun getGifsTrending(): PagingSource<Int, Gif>
+    fun getFavoriteGifs(): PagingSource<Int, Gif>
+    suspend fun updateGif(gif: Gif): Int
+    suspend fun clearGifs()
+
+    /* RemoteKeysDao */
+    suspend fun saveRemoteKeys(remoteKey: List<RemoteKeys>)
+    suspend fun getRemoteKeysWithId(gifId: String): RemoteKeys?
+    suspend fun clearRemoteKeys()
 }
---
+++ app/src/main/java/m/woong/giphysample/data/source/local/LocalDataSourceImpl.kt
@@ -0,0 +1,53 @@
+package m.woong.giphysample.data.source.local
+
+import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
+import androidx.paging.PagedList
+import androidx.paging.PagingSource
+import androidx.paging.toLiveData
+import kotlinx.coroutines.flow.Flow
+import m.woong.giphysample.data.source.local.dao.GifDao
+import m.woong.giphysample.data.source.local.dao.RemoteKeysDao
+import m.woong.giphysample.data.source.local.entity.Gif
+import m.woong.giphysample.data.source.local.entity.RemoteKeys
+import javax.inject.Inject
+
+class LocalDataSourceImpl @Inject constructor(
+    private val gifDao: GifDao,
+    private val remoteKeysDao: RemoteKeysDao
+): LocalDataSource {
+
+    /* GifDao */
+    override suspend fun saveGifs(gifs: List<Gif>) {
+        gifDao.insertGifs(gifs)
+    }
+
+    override fun getGifsTrending(): PagingSource<Int, Gif> {
+        return gifDao.selectGifs()
+    }
+
+    override fun getFavoriteGifs(): PagingSource<Int, Gif> {
+        return gifDao.selectFavoriteGifs()
+    }
+
+    override suspend fun updateGif(gif: Gif): Int {
+        return gifDao.updateGif(gif)
+    }
+
+    override suspend fun clearGifs() {
+        gifDao.clearGifs()
+    }
+
+    /* RemoteKeysDao */
+    override suspend fun saveRemoteKeys(remoteKey: List<RemoteKeys>) {
+        remoteKeysDao.insertKeys(remoteKey)
+    }
+
+    override suspend fun getRemoteKeysWithId(gifId: String): RemoteKeys? {
+        return remoteKeysDao.selectKeysGifId(gifId)
+    }
+
+    override suspend fun clearRemoteKeys() {
+        remoteKeysDao.clearKeys()
+    }
+}
