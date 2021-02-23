a93b238fad230300051822c20e44881842b3fc00
dlqkfzlr <hisunwoong@gmail.com>
2021-02-23 21:48:29 +0900

    changed method (onActivityCreated to onViewCreated) in FavoritesFragment.kt

--- app/src/main/java/m/woong/giphysample/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/m/woong/giphysample/ui/favorites/FavoritesFragment.kt
@@ -7,24 +7,29 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import m.woong.giphysample.R
+import m.woong.giphysample.data.source.local.entity.Gif
 import m.woong.giphysample.databinding.FavoritesFragmentBinding
 import m.woong.giphysample.ui.MainViewModel
 import m.woong.giphysample.ui.adapter.FavoriteGifAdapter
+import m.woong.giphysample.ui.adapter.TrendingGifToggleListener
 import m.woong.giphysample.utils.setGridLayoutManager
 
 @AndroidEntryPoint
-class FavoritesFragment : Fragment() {
+class FavoritesFragment : Fragment(), TrendingGifToggleListener {
 
     private val viewModel: MainViewModel by activityViewModels()
     private lateinit var binding: FavoritesFragmentBinding
     private lateinit var fAdapter: FavoriteGifAdapter
     private var favoriteJob: Job? = null
+    private var _listener: TrendingGifToggleListener? = null
+    private val listener get() = _listener!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,17 +37,23 @@ class FavoritesFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.favorites_fragment, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
+        _listener = this
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         initAdapter()
         initFavorite()
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _listener = null
+    }
+
     private fun initAdapter() {
-        fAdapter = FavoriteGifAdapter()
+        fAdapter = FavoriteGifAdapter(listener)
         binding.rvFavorites.adapter = fAdapter
         binding.rvFavorites.apply {
             setGridLayoutManager(2)
@@ -57,6 +68,17 @@ class FavoritesFragment : Fragment() {
                 fAdapter.submitData(it)
             }
         }
+        viewModel.update.observe(viewLifecycleOwner,
+        Observer {
+            it.getContentIfNotHandled()?.let {
+//                fAdapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+    override fun onToggleFavorite(gif: Gif) {
+        gif.isFavorite = !gif.isFavorite
+        viewModel.saveFavoriteGif(gif)
     }
 
     companion object {
